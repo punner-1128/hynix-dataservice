992edfbb1866f2480489e9d2b366a9a9da85bfe3
uselessdog <uselessdog@gmx.com>
2026-03-02 16:29:26 +0900

    mongo filter $gte, $lte 조건 대응 수정

--- src/main/java/com/example/amos/service/repository/mongo/MongoDb1Repository.java
+++ src/main/java/com/example/amos/service/repository/mongo/MongoDb1Repository.java
@@ -2,6 +2,9 @@ package com.example.amos.service.repository.mongo;
 
 import java.util.List;
 import java.util.Map;
+import java.time.Instant;
+import java.time.format.DateTimeParseException;
+import java.util.Date;
 
 import org.bson.Document;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -32,8 +35,59 @@ public class MongoDb1Repository {
 
         Query query = new Query();
         for (Map.Entry<String, Object> entry : filter.entrySet()) {
-            query.addCriteria(Criteria.where(entry.getKey()).is(entry.getValue()));
+            Criteria criteria = Criteria.where(entry.getKey());
+            Object value = entry.getValue();
+            boolean hasOperator = false;
+
+            if (value instanceof Map<?, ?> operatorMap) {
+                for (Map.Entry<?, ?> opEntry : operatorMap.entrySet()) {
+                    if (!(opEntry.getKey() instanceof String operator)) {
+                        continue;
+                    }
+                    Object converted = convertValue(opEntry.getValue());
+                    switch (operator) {
+                        case "$gte" -> {
+                            criteria.gte(converted);
+                            hasOperator = true;
+                        }
+                        case "$lte" -> {
+                            criteria.lte(converted);
+                            hasOperator = true;
+                        }
+                        case "$gt" -> {
+                            criteria.gt(converted);
+                            hasOperator = true;
+                        }
+                        case "$lt" -> {
+                            criteria.lt(converted);
+                            hasOperator = true;
+                        }
+                        case "$eq" -> {
+                            criteria.is(converted);
+                            hasOperator = true;
+                        }
+                        default -> {
+                        }
+                    }
+                }
+            }
+
+            if (!hasOperator) {
+                criteria.is(convertValue(value));
+            }
+            query.addCriteria(criteria);
         }
         return (List<Map<String, Object>>) (List<?>) mongoTemplate.find(query, Map.class, collection);
+    }
+
+    private Object convertValue(Object value) {
+        if (value instanceof String str) {
+            try {
+                return Date.from(Instant.parse(str));
+            } catch (DateTimeParseException ignored) {
+                return str;
+            }
+        }
+        return value;
     }
 }
