7238561838efbde632a5bda6e19355cd311c67d8
uselessdog <uselessdog@gmx.com>
2026-03-09 21:25:20 +0900

    Oracle로직 제거

--- src/main/java/com/example/amos/service/AmosServiceApplication.java
+++ src/main/java/com/example/amos/service/AmosServiceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfigurat
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.retry.annotation.EnableRetry;
 
-@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
+@SpringBootApplication
 @EnableRetry
 public class AmosServiceApplication {
 
--- src/main/java/com/example/amos/service/repository/logpresso/LogpressoClient.java
+++
@@ -1,6 +0,0 @@
-package com.example.amos.service.repository.logpresso;
-
-public interface LogpressoClient {
-
-    String health();
-}
